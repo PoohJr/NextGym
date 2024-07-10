274ed3b1231e643226693cd0c32f880bc48e19b2
PoohJr <Williamdomenech06@gmail.com>
2024-07-10 13:46:15 -0400

    nm

---
+++ app/home/client.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from 'react'
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+const client = () => {
+
+    useGSAP(() => {
+
+    })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default client
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
+import Client from './client'
 import Image from 'next/image'
 import Img1 from './images/gym1.jpg'
 import Img2 from './images/gym2.jpg'
 import Img3 from './images/gym3.jpg'
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 
 
-gsap.registerPlugin(useGSAP);
+
 
 const HomePage = () => {
-  use
+
+
 
   return (
-    <div className='bg-black w-full h-screen'>
-
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full h-full">
+    <div className="bg-black w-full h-screen">
+    <div className='flex justify-between '>
+      <div className="flex items-center text-white font-bold ">
+        Welcome To The Collose Gym Partner
+      </div>
+    <div className=" carousel w-96 h-screen">
+      <div id="slide1" className="carousel-item relative w-full h-full backdrop-blur-lg">
+      <div className="absolute inset-0 backdrop-blur-lg z-0"></div>
         <Image
           src={Img1}
           alt='Image of Dumbell'
-          className="w-full" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          className="relative z-10 w-full" />
+
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between z-20">
           <a href="#slide4" className="btn btn-circle">❮</a>
           <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
@@ -48,6 +53,7 @@ const HomePage = () => {
       </div>
     </div>
         
+  </div>
   </div>
   // end of contaiter div ^^
   )
