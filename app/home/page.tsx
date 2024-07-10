b36412954557f71537c24552e2717b8d96e4128c
PoohJr <Williamdomenech06@gmail.com>
2024-07-10 10:48:48 -0400

    new

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,22 +3,53 @@ import Image from 'next/image'
 import Img1 from './images/gym1.jpg'
 import Img2 from './images/gym2.jpg'
 import Img3 from './images/gym3.jpg'
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+
+gsap.registerPlugin(useGSAP);
 
 const HomePage = () => {
-    const imgs =[Img1, Img2, Img3]
+  use
+
   return (
     <div className='bg-black w-full h-screen'>
 
-        {imgs.map((pic, index) => (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full h-full">
         <Image
-        key={index}
-        className='w-full'
-        src={pic}
-        alt="it is a pic of Home screen"
-        />
-        ))}
-        
+          src={Img1}
+          alt='Image of Dumbell'
+          className="w-full" />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide4" className="btn btn-circle">❮</a>
+          <a href="#slide2" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+      <Image
+          src={Img2}
+          alt='Image of Gym Man'
+          className="w-full" />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide1" className="btn btn-circle">❮</a>
+          <a href="#slide3" className="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+      <Image
+          src={Img3}
+          alt='Image of Strong Independent Women'
+          className="w-full" />
+        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+          <a href="#slide2" className="btn btn-circle">❮</a>
+          <a href="#slide4" className="btn btn-circle">❯</a>
+        </div>
+      </div>
     </div>
+        
+  </div>
+  // end of contaiter div ^^
   )
 }
 
