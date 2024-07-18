ed350981d3c97900a69c32685b9c4ead3aed3577
PoohJr <Williamdomenech06@gmail.com>
2024-07-18 10:50:24 -0400

    updated

--- app/trainer/page.tsx
+++ app/trainer/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import Bill from "./trainersimg/bill.jpg";
 
 const Trainpage = () => {
-  return <div>This IS TRAIMER PAGE</div>;
+  const trainersImg = [];
+  const trainersName = ["Bill", "Zoey", "Francis", "Louis"];
+
+  return (
+    <div className="p-8">
+      <div className="p-10">
+        <p className="text-center text-3xl text-white font-body">
+          Hello and Welcome To the
+        </p>
+        <p className="text-center text-3xl text-white font-body">
+          Trainers of (nameofgym)
+        </p>
+      </div>
+      {Bill}
+    </div>
+  );
 };
 
 export default Trainpage;
