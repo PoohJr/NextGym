d472b8faf253f25684e6ca1d97e37064b037630c
PoohJr <Williamdomenech06@gmail.com>
2024-07-20 12:59:02 -0400

    got some things updated

--- app/navbar/NavBar.tsx
+++ app/navbar/NavBar.tsx
@@ -1,31 +1,27 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 const NavBar = () => {
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 dark:bg-transparent">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a
             href="https://flowbite.com"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
+            {/* Place Img Here */}
+            {/* <Image
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-8"
               alt="Flowbite Logo"
-            />
+            /> */}
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              (Gymname)
             </span>
           </a>
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
-            <a
-              href="tel:5541251234"
-              className="text-sm  text-gray-500 dark:text-white hover:underline"
-            >
-              (123) 456-7890
-            </a>
-            <Link href="#">
+            <Link href="./joinus">
               <button className="text-white font-body font-bold ">
                 Join Us
               </button>
@@ -33,42 +29,44 @@ const NavBar = () => {
           </div>
         </div>
       </nav>
-      <nav className="bg-gray-50 dark:bg-gray-700">
-        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+      <nav className="bg-gray-150 dark:bg-gray-700">
+        <div className="bg-black max-w-screen-xl px-4 py-3 mx-auto">
           <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
                 <Link
                   href="/"
-                  className="text-gray-900 dark:text-white hover:underline"
+                  className="text-white dark:text-white hover:underline"
                   aria-current="page"
                 >
-                  Classes
+                  Home
                 </Link>
               </li>
+
               <li>
-                <a
-                  href="#"
+                <Link
                   className="text-gray-900 dark:text-white hover:underline"
+                  href="/newabout"
                 >
-                  Rates
-                </a>
+                  About Us
+                </Link>
               </li>
+
               <li>
                 <Link
                   href="./trainer"
                   className="text-gray-900 dark:text-white hover:underline"
                 >
-                  Training
+                  Classes
                 </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
                   className="text-gray-900 dark:text-white hover:underline"
+                  href="/membership"
                 >
-                  Features
-                </a>
+                  MemberShip
+                </Link>
               </li>
             </ul>
           </div>
--- app/trainer/page.tsx
+++ app/trainer/page.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import Image from "next/image";
 import Bill from "../public/teach-img/Bill.jpg";
 import Zoey from "../public/teach-img/Zoey.jpg";
+import Francis from "../public/teach-img/Francis.jpg";
+import Luis from "../public/teach-img/Luis.jpg";
 
 const Trainpage = () => {
-  const trainersImg = [Bill, Zoey];
+  // Decorate this more but this is the foundation
+  const trainersImg = [Bill, Zoey, Francis, Luis];
   const trainersName = ["Bill", "Zoey", "Francis", "Louis"];
+  const trainersDesc = [
+    "Bill is Great boxing Coach",
+    "Zoey is a great Yoga Instructer",
+    "our best strength coaxh is the one and only Francis",
+    "our swimming coach is luis ",
+  ];
 
   return (
     <div className="p-8">
@@ -17,8 +26,18 @@ const Trainpage = () => {
           Trainers of (nameofgym)
         </p>
       </div>
-      <div className="w-96 h-96">
-        <Image src={Bill} alt="" />
+      <div className="flex flex-col justify-center  items-center">
+        {trainersImg.map((img, index) => (
+          <div className="w-auto h-auto m-10 p-10" key={index}>
+            <Image className="w-96 h-96" src={img} alt="hello" />
+            <p className="text-center font-body text-white text-2xl">
+              {trainersName[index]}
+            </p>
+            <p className="text-center p-3 font-body text-white">
+              {trainersDesc[index]}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
